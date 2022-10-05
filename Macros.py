51f85e81a71c0525fb377ef3c558ecfa07b67e7e
Sean <sean.fletcher17@gmail.com>
2022-10-05 13:33:06 -0400

    Drunken Sailor Complete Prototyping for Mouse movement

---
+++ Macros.py
@@ -0,0 +1,32 @@
+import keyboard;
+import time;
+import random;
+import mouse;
+
+
+
+def drunkenSailor():
+    timer = random.uniform(.1, 2)
+    print("Time dif: " + str(timer))
+    stop = time.time() + timer
+    print("going left")
+    while(time.time() < stop):
+        keyboard.press("a")
+        time.sleep(.1)
+    print("stopping")
+    keyboard.release("a")
+
+def insaneRecoil():
+    print("waiting")
+    time.sleep(5)
+    print("recording starting")
+    recording = mouse.record(button='right')
+    print("recording stopped, playing")
+    mouse.play(recording, speed_factor=2.0, include_clicks=True, include_moves=True, include_wheel=True)
+    mouse.right_click()
+
+while(1):
+    insaneRecoil()
+    print("sleeping")
+    time.sleep(5)
+
