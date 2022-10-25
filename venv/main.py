aaf0bb82ba34591d394b84986b15c99a63339fa0
Sean <sean.fletcher17@gmail.com>
2022-10-25 14:36:18 -0400

    Switched to multiprocessing vs threads Debuffs now run together correctly, and terminate with no errors Issue: Cannot restart process once it is used

--- venv/main.py
+++ venv/main.py
@@ -1,117 +1,103 @@
 import tkinter as tk
 from tkinter import *
 from Macros import *
-import threading
+import multiprocessing
 import time
 
-
-#creating the window
-window = tk.Tk()
-window.geometry("500x500")
-window.title("Destiny Master Grind")
-
-
-
-#Selection checkboxes
-drunkenBool = tk.BooleanVar()
-drunkenCheck = tk.Checkbutton(window, text = "Drunken Sailor", variable=drunkenBool, onvalue = True )
-drunkenCheck.grid(row = 0, column = 0,)
-
-fatBool = tk.BooleanVar()
-fatCheck = tk.Checkbutton(window, text = "Fat Fingered", variable=fatBool, onvalue = True )
-fatCheck.grid(row = 1, column = 0,)
-
-indecisiveBool = tk.BooleanVar()
-indecisiveCheck = tk.Checkbutton(window, text = "Indecisive", variable=indecisiveBool, onvalue = True )
-indecisiveCheck.grid(row = 2, column = 0,)
-
-dancingBool = tk.BooleanVar()
-dancingCheck = tk.Checkbutton(window, text = "Random Dancing", variable=dancingBool, onvalue = True )
-dancingCheck.grid(row = 3, column = 0,)
-
-tripBool = tk.BooleanVar()
-tripCheck = tk.Checkbutton(window, text = "Tripping", variable=tripBool, onvalue = True )
-tripCheck.grid(row = 4, column = 0,)
-
-hopsBool = tk.BooleanVar()
-hopsCheck = tk.Checkbutton(window, text = "Hops", variable=hopsBool, onvalue = True )
-hopsCheck.grid(row = 5, column = 0,)
-
-reloadBool = tk.BooleanVar()
-reloadCheck = tk.Checkbutton(window, text = "Compulsive Reload", variable=reloadBool, onvalue = True )
-reloadCheck.grid(row = 6, column = 0,)
-
 #create macro threads
-drukenEvent = threading.Event()
-drunkenThread = threading.Thread(target=drunkenSailor, args=(drukenEvent,))
-drunkenThread.daemon = True
+drunkenProcess = multiprocessing.Process(target=drunkenSailor)
 
-fatEvent = threading.Event()
-fatThread = threading.Thread(target=fatFingered, args=(fatEvent,))
-fatThread.daemon = True
+fatProcess = multiprocessing.Process(target=fatFingered)
 
-indecisiveEvent = threading.Event()
-indecisiveThread = threading.Thread(target=indecisive, args=(indecisiveEvent,))
-indecisiveThread.daemon = True
+indecisiveProcess = multiprocessing.Process(target=indecisive)
 
-dancingEvent = threading.Event()
-dancingThread = threading.Thread(target=randomDancing, args=(dancingEvent,))
-dancingThread.daemon = True
+dancingProcess = multiprocessing.Process(target=randomDancing)
 
-tripEvent = threading.Event()
-tripThread = threading.Thread(target=tripping, args=(tripEvent,))
-tripThread.daemon = True
+tripProcess = multiprocessing.Process(target=tripping)
 
-hopsEvent = threading.Event()
-hopsThread = threading.Thread(target=hops, args=(hopsEvent,))
-hopsThread.daemon = True
+hopsProcess= multiprocessing.Process(target=hops)
 
-reloadEvent = threading.Event()
-reloadThread = threading.Thread(target=compulsiveReload, args=(reloadEvent,))
-hopsThread.daemon = True
+reloadProcess = multiprocessing.Process(target=compulsiveReload)
+
 
 #Apply Selection Button
 def applyButtonAction():
     if(drunkenBool.get()):
-        drunkenThread.start()
+        drunkenProcess.start()
     if(fatBool.get()):
-        fatThread.start()
+        fatProcess.start()
     if(indecisiveBool.get()):
-        indecisiveThread.start()
+        indecisiveProcess.start()
     if(dancingBool.get()):
-        dancingThread.start()
+        dancingProcess.start()
     if(tripBool.get()):
-        tripThread.start()
+        tripProcess.start()
     if (hopsBool.get()):
-        hopsThread.start()
+        hopsProcess.start()
     if (reloadBool.get()):
-        reloadThread.start()
+        reloadProcess.start()
 
 def stopButtonAction():
-    drukenEvent.set()
-    fatEvent.set()
-    indecisiveEvent.set()
-    dancingEvent.set()
-    tripEvent.set()
-    hopsEvent.set()
-    reloadEvent.set()
-    print("stopped threads")
-    time.sleep(1)
-    drukenEvent.clear()
-    fatEvent.clear()
-    indecisiveEvent.clear()
-    dancingEvent.clear()
-    tripEvent.clear()
-    hopsEvent.clear()
-    reloadEvent.set()
-    print("events cleared")
+    if(drunkenProcess.is_alive()):
+        drunkenProcess.terminate()
+    if(fatProcess.is_alive()):
+        fatProcess.terminate()
+    if(indecisiveProcess.is_alive()):
+        indecisiveProcess.terminate()
+    if(dancingProcess.is_alive()):
+        dancingProcess.terminate()
+    if(tripProcess.is_alive()):
+        tripProcess.terminate()
+    if(hopsProcess.is_alive()):
+        hopsProcess.terminate()
+    if(reloadProcess.is_alive()):
+        reloadProcess.terminate()
+    print("Processes Killed")
 
-applyButton = tk.Button(window, text="Apply", command= lambda : applyButtonAction())
-applyButton.grid(row=20, column=2)
+if __name__ == '__main__':
+    #creating the window
+    window = tk.Tk()
+    window.geometry("500x500")
+    window.title("Destiny Master Grind")
 
-stopButton = tk.Button(window, text="Stop", command= lambda : stopButtonAction())
-stopButton.grid(row=20, column=3)
+    #Selection checkboxes
 
-#Making the program run
-window.mainloop()
+    drunkenBool = tk.BooleanVar()
+    drunkenCheck = tk.Checkbutton(window, text = "Drunken Sailor", variable=drunkenBool, onvalue = True )
+    drunkenCheck.grid(row = 0, column = 0,)
+
+    fatBool = tk.BooleanVar()
+    fatCheck = tk.Checkbutton(window, text = "Fat Fingered", variable=fatBool, onvalue = True )
+    fatCheck.grid(row = 1, column = 0,)
+
+    indecisiveBool = tk.BooleanVar()
+    indecisiveCheck = tk.Checkbutton(window, text = "Indecisive", variable=indecisiveBool, onvalue = True )
+    indecisiveCheck.grid(row = 2, column = 0,)
+
+    dancingBool = tk.BooleanVar()
+    dancingCheck = tk.Checkbutton(window, text = "Random Dancing", variable=dancingBool, onvalue = True )
+    dancingCheck.grid(row = 3, column = 0,)
+
+    tripBool = tk.BooleanVar()
+    tripCheck = tk.Checkbutton(window, text = "Tripping", variable=tripBool, onvalue = True )
+    tripCheck.grid(row = 4, column = 0,)
+
+    hopsBool = tk.BooleanVar()
+    hopsCheck = tk.Checkbutton(window, text = "Hops", variable=hopsBool, onvalue = True )
+    hopsCheck.grid(row = 5, column = 0,)
+
+    reloadBool = tk.BooleanVar()
+    reloadCheck = tk.Checkbutton(window, text = "Compulsive Reload", variable=reloadBool, onvalue = True )
+    reloadCheck.grid(row = 6, column = 0,)
+
+
+    applyButton = tk.Button(window, text="Apply", command= lambda : applyButtonAction())
+    applyButton.grid(row=20, column=2)
+
+    stopButton = tk.Button(window, text="Stop", command= lambda : stopButtonAction())
+    stopButton.grid(row=20, column=3)
+
+    #Making the program run
+    window.mainloop()
+
 
