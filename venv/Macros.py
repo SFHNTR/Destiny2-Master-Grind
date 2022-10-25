773c4b665abc6258e85535ca188b8a3536e096af
Sean <sean.fletcher17@gmail.com>
2022-10-25 13:42:07 -0400

    Random Dancing Completed Tripping Completed Hops Completed Fixed an issue with key commands not being released, causing the keyboard to crash

--- venv/Macros.py
+++ venv/Macros.py
@@ -38,10 +38,13 @@ def fatFingered(event):
             fiftyFifty = random.choice([1,2])
             if (fiftyFifty == 1):
                 keyboard.press("f")
+                time.sleep(.1)
+                keyboard.release("f")
             timer = 10
             testTime = time.time() + timer
         if event.isSet():
             break
+
 def indecisive(event):
     timer = random.uniform(15, 30)
     switchTime = time.time() + timer
@@ -50,12 +53,62 @@ def indecisive(event):
             weaponSelect = random.choice([1, 2, 3])
             if (weaponSelect == 1):
                 keyboard.press("1")
+                time.sleep(.1)
+                keyboard.release("1")
             elif (weaponSelect == 2):
                 keyboard.press("2")
+                time.sleep(.1)
+                keyboard.release("2")
             elif (weaponSelect == 3):
                 keyboard.press("3")
+                time.sleep(.1)
+                keyboard.release("3")
 
             timer = random.uniform(15, 30)
             switchTime = time.time() + timer
+        if event.isSet():
+            break
+
+def randomDancing(event):
+    print("dancing start")
+    timer = random.uniform(20, 30)
+    danceTime = time.time() + timer
+    while(1):
+        if (time.time() > danceTime):
+            danceSelect = random.choice(["up", "down", "left", "right"])
+            keyboard.press(danceSelect)
+            time.sleep(.1)
+            keyboard.release(danceSelect)
+            timer = random.uniform(20, 30)
+            danceTime = time.time() + timer
+        if event.isSet():
+            break
+
+def tripping(event):
+    timer = random.uniform(10, 45)
+    tripTime = time.time() + timer
+    while(1):
+        if (time.time() > tripTime):
+            keyboard.press("ctrl")
+            time.sleep(.1)
+            keyboard.release("ctrl")
+            timer = random.uniform(10, 45)
+            tripTime = time.time() + timer
+        if event.isSet():
+            break
+
+def hops(event):
+    print("hops starting")
+    timer = random.uniform(1, 2)
+    hopsTime = time.time() + timer
+    while(1):
+        if (time.time() > hopsTime):
+            print("hopping")
+            keyboard.press("space")
+            time.sleep(.1)
+            keyboard.release("space")
+            timer = random.uniform(1, 2)
+            hopsTime = time.time() + timer
+
         if event.isSet():
             break
--- venv/main.py
+++ venv/main.py
@@ -25,6 +25,18 @@ indecisiveBool = tk.BooleanVar()
 indecisiveCheck = tk.Checkbutton(window, text = "Indecisive", variable=indecisiveBool, onvalue = True )
 indecisiveCheck.grid(row = 2, column = 0,)
 
+dancingBool = tk.BooleanVar()
+dancingCheck = tk.Checkbutton(window, text = "Random Dancing", variable=dancingBool, onvalue = True )
+dancingCheck.grid(row = 3, column = 0,)
+
+tripBool = tk.BooleanVar()
+tripCheck = tk.Checkbutton(window, text = "Tripping", variable=tripBool, onvalue = True )
+tripCheck.grid(row = 4, column = 0,)
+
+hopsBool = tk.BooleanVar()
+hopsCheck = tk.Checkbutton(window, text = "Hops", variable=hopsBool, onvalue = True )
+hopsCheck.grid(row = 5, column = 0,)
+
 #create macro threads
 drukenEvent = threading.Event()
 drunkenThread = threading.Thread(target=drunkenSailor, args=(drukenEvent,))
@@ -37,6 +49,19 @@ fatThread.daemon = True
 indecisiveEvent = threading.Event()
 indecisiveThread = threading.Thread(target=indecisive, args=(indecisiveEvent,))
 indecisiveThread.daemon = True
+
+dancingEvent = threading.Event()
+dancingThread = threading.Thread(target=randomDancing, args=(dancingEvent,))
+dancingThread.daemon = True
+
+tripEvent = threading.Event()
+tripThread = threading.Thread(target=tripping, args=(tripEvent,))
+tripThread.daemon = True
+
+hopsEvent = threading.Event()
+hopsThread = threading.Thread(target=hops, args=(hopsEvent,))
+hopsThread.daemon = True
+
 #Apply Selection Button
 def applyButtonAction():
     if(drunkenBool.get()):
@@ -45,16 +70,28 @@ def applyButtonAction():
         fatThread.start()
     if(indecisiveBool.get()):
         indecisiveThread.start()
+    if(dancingBool.get()):
+        dancingThread.start()
+    if(tripBool.get()):
+        tripThread.start()
+    if (hopsBool.get()):
+        hopsThread.start()
 
 def stopButtonAction():
     drukenEvent.set()
     fatEvent.set()
     indecisiveEvent.set()
+    dancingEvent.set()
+    tripEvent.set()
+    hopsEvent.set()
     print("stopped threads")
     time.sleep(1)
     drukenEvent.clear()
     fatEvent.clear()
     indecisiveEvent.clear()
+    dancingEvent.clear()
+    tripEvent.clear()
+    hopsEvent.clear()
     print("events cleared")
 
 applyButton = tk.Button(window, text="Apply", command= lambda : applyButtonAction())
