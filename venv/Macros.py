163c5597546d2bded36c9e8971510ce54a8e297f
Sean <sean.fletcher17@gmail.com>
2022-10-25 12:57:54 -0400

    Drunken Sailor moved to different script Fat Fingered Completed

--- venv/Macros.py
+++ venv/Macros.py
@@ -1,21 +1,25 @@
-import keyboard;
-import time;
-import random;
-import mouse;
+import keyboard
+import time
+import random
+import mouse
 
 
-def drunkenSailor(window):
+def drunkenSailor(event):
     while(1):
         timer = random.uniform(.1, 2)
-        print("Time dif: " + str(timer))
         stop = time.time() + timer
-        print("going left")
+        wasdList = ["w", "a", "s", "d" ]
+        selectedInput = random.choice(wasdList)
+        print(selectedInput)
         while(time.time() < stop):
-            keyboard.press("a")
-            time.sleep(.1)
+            keyboard.press(selectedInput)
+            time.sleep(.5)
         print("stopping")
-        keyboard.release("a")
-
+        keyboard.release(selectedInput)
+        if event.isSet():
+            print("Drunken Thread Killed")
+            break
+    return
 
 def insaneRecoil():
     print("waiting")
@@ -26,4 +30,15 @@ def insaneRecoil():
     mouse.play(recording, speed_factor=2.0, include_clicks=True, include_moves=True, include_wheel=True)
     mouse.right_click()
 
-
+def fatFingered(event):
+    timer = 45
+    testTime = time.time() + timer
+    while(1):
+        if(time.time() > testTime):
+            fiftyFifty = random.choice([1,2])
+            if (fiftyFifty == 1):
+                keyboard.press("f")
+            timer = 10
+            testTime = time.time() + timer
+        if event.isSet():
+            break
--- venv/main.py
+++ venv/main.py
@@ -1,12 +1,9 @@
 import tkinter as tk
 from tkinter import *
-#from Macros import drunkenSailor
+from Macros import *
 import threading
+import time
 
-import keyboard;
-import time;
-import random;
-import mouse;
 
 #creating the window
 window = tk.Tk()
@@ -20,40 +17,40 @@ drunkenBool = tk.BooleanVar()
 drunkenCheck = tk.Checkbutton(window, text = "Drunken Sailor", variable=drunkenBool, onvalue = True )
 drunkenCheck.grid(row = 0, column = 0,)
 
+fatBool = tk.BooleanVar()
+fatCheck = tk.Checkbutton(window, text = "Fat Fingered", variable=fatBool, onvalue = True )
+fatCheck.grid(row = 1, column = 0,)
 
-#create macros
-def drunkenSailor(event):
-    while(1):
-        timer = random.uniform(.1, 2)
-        stop = time.time() + timer
-        wasdList = ["w", "a", "s", "d" ]
-        selectedInput = random.choice(wasdList)
-        print(selectedInput)
-        while(time.time() < stop):
-            keyboard.press(selectedInput)
-            time.sleep(.1)
-        print("stopping")
-        keyboard.release(selectedInput)
-        if event.isSet():
-            print("Drunken Thread Killed")
-            break
-
-
+#create macro threads
 drukenEvent = threading.Event()
 drunkenThread = threading.Thread(target=drunkenSailor, args=(drukenEvent,))
 drunkenThread.daemon = True
 
+fatEvent = threading.Event()
+fatThread = threading.Thread(target=fatFingered, args=(fatEvent,))
+fatThread.daemon = True
 
 #Apply Selection Button
 def applyButtonAction():
     if(drunkenBool.get()):
         drunkenThread.start()
-    else:
-        drukenEvent.set()
+    if(fatBool.get()):
+        fatThread.start()
 
+def stopButtonAction():
+    drukenEvent.set()
+    fatEvent.set()
+    print("stopped threads")
+    time.sleep(1)
+    drukenEvent.clear()
+    fatEvent.clear()
+    print("events cleared")
 
 applyButton = tk.Button(window, text="Apply", command= lambda : applyButtonAction())
 applyButton.grid(row=2, column=2)
+
+stopButton = tk.Button(window, text="Stop", command= lambda : stopButtonAction())
+stopButton.grid(row=2, column=3)
 
 #Making the program run
 window.mainloop()
